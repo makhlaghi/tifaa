0a654d480e7e841d900f130a5f7ea1b9880f2a70
Mohammad Akhlaghi <makhlaghi@gmail.com>
2013-07-23 19:49:32 +0900

    Update tifaa.c

--- src/tifaa.c
+++ src/tifaa.c
@@ -1,12 +1,26 @@
-/*********************************************
- *********************************************
- *********************************************
- **          Mohammad Akhalaghi             **
- **   http://astr.tohoku.ac.jp/~akhlaghi/   **
- **              July 2013                  **
- *********************************************
- *********************************************
- *********************************************/
+/*********************************************************************
+tifaa - Thumbnail images from astronomical archives
+A simple set of functions to crop thumbnails from astronomical archives.
+
+Copyright (C) 2013 Mohammad Akhlaghi
+Tohoku University Astronomical Institute, Sendai, Japan.
+http://astr.tohoku.ac.jp/~akhlaghi/
+
+tifaa is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+tifaa is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+**********************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "attaavv.h"
